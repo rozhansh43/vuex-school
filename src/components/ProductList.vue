29a6a19e1f45cba8c429e26a1e42d054ba22a601
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-17 13:09:07 +0430

    first lesson finished with error

---
+++ src/components/ProductList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>
+      Product List
+    </h1>
+
+    <img 
+    v-if="loaded"
+    src="https://i.imgur.com/JfPpwOA.gif" 
+    alt=""
+    >
+
+    <ul v-else>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - {{ product.price }}
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+// import store from '@/store/index.js'
+
+export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+  computed: {
+    products () {
+      return $store.getters.availableProducts
+    }
+  },
+  created () {
+    this.loading = true
+    store.dispatch('fetchProducts')
+      .then(() => this.loading = false )
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,44 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import shop from '@/api/shop'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products:[],
+    cart: []
+  },
+  getters: {
+    availableProducts (state, getters) {
+      return state.products.filter(product => product => product.inventory > 0)
+    }
+  },
+  mutations: {
+    setProducts (state,products) {
+      state.products = products
+    }
+  },
+  actions: {
+    fetchProducts ({commit}) {
+      return new promise ((resolve,reject) => {
+        shop.getProducts ( products => {
+          commit ('setProducts', products)
+          resolve ()
+        })
+      })
+    },
+    addProductToCart (context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id )
+        if (!carIitem) {
+          context.commit ('pushProductToCart', product) 
+        }else {
+          context.commit('increamentItemQuantitiy', cartItem)
+        }
+      }
+     
+    }
+  },
   modules: {},
 });
